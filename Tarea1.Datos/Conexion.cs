ea8769779c399241535751c161c9b9b3f490e061
OsEstrada <00064318@uca.edu.sv>
2021-04-10 22:50:18 -0600

    Clase Conexion creada

---
+++ Tarea1.Datos/Conexion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1.Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private bool Seguridad;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "Tarea1";
+            this.Servidor = "LAPTOP-RRVJQMV6\\SQLEXPRESS";
+            this.Usuario = "sa";
+            this.Clave = "rootx10";
+            this.Seguridad = true; //True = windonws False = sql Auth
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = $"Server={this.Servidor}; Database={this.Base}; ";
+                if (this.Seguridad) //windows
+                {
+                    Cadena.ConnectionString += "Intregated Security = SSPI";
+                }
+                else //sql
+                {
+                    Cadena.ConnectionString += $"User Id={this.Usuario}; Password={this.Clave}";
+                }
+            }catch(Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if(Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
